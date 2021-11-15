2a08b5ae712977b560ba12b28e5129fd315e5637
joaotuliojt <joaotuliosoares@hotmail.com>
2021-11-15 14:51:17 -0300

    :tada: Challenge Finalized

--- src/index.js
+++ src/index.js
@@ -10,26 +10,72 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const {username} = request.headers;
+  const user = users.find((user) => user.username === username);
+
+  if(!user){
+    return response.status(404).json({error: "conta não existente"})
+  }
+
+  request.user = user;
+  next();
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const {user} = request;
+  
+  if(!user.pro && user.todos.length >= 10){
+    return response.status(403).json({error: "limite máximo de tarefas atingido"})
+  }
+
+  next();  
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const {username} = request.headers;
+  const { id } = request.params;
+
+  const isId = validate(id);
+
+  if(!isId){
+    return response.status(400).json({error: "ID inválido"})
+  }
+
+  const user = users.find((user) => user.username === username);
+
+  if(!user){
+    return response.status(404).json({error: "Usuário não encontrado"})
+  }
+
+  const todo = user.todos.find((todo) => todo.id === id)
+
+  if(!todo){
+    return response.status(404).json({error: "Tarefa não encontrada"})
+  }
+
+  request.user = user;
+  request.todo = todo;
+  next()
 }
 
 function findUserById(request, response, next) {
   // Complete aqui
+  const { id } = request.params;
+
+  const user = users.find(user => user.id === id);
+
+  if(!user){
+    return response.status(404).json({error: "usuário não encontrado"})
+  }
+
+  request.user = user;
+  next();
 }
 
 app.post('/users', (request, response) => {
   const { name, username } = request.body;
 
   const usernameAlreadyExists = users.some((user) => user.username === username);
-
   if (usernameAlreadyExists) {
     return response.status(400).json({ error: 'Username already exists' });
   }
